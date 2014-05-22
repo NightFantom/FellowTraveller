29cc5872eb793e7eb453447f6d23a8caa377da41
Denis <Den1-1@mail.ru>
2014-05-22 20:09:22 +0400

    Изменены настройки Hibernate. Теперь класс  статичный

--- AdminPanel/src/hibernateServise/HibernateUtil.java
+++ AdminPanel/src/hibernateServise/HibernateUtil.java
@@ -1,53 +1,44 @@
 package hibernateServise;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.SessionFactoryObserver;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    private  final SessionFactory sessionFactory = buildSessionFactory();
-    private ServiceRegistry serviceRegistry;
-    private  final ThreadLocal session = new ThreadLocal();
-
-
-
-    private SessionFactory buildSessionFactory() {
-        SessionFactory sf;
+    private static SessionFactory buildSessionFactory() {
         try {
-            Configuration configuration = new Configuration().configure();
-            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            sf = configuration.buildSessionFactory(serviceRegistry);
-        } catch (Throwable ex) {
+            // Create the SessionFactory from hibernate.cfg.xml
+            Configuration configuration = new Configuration().configure("hibernateServise/hibernate.cfg.xml");
+            final ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            configuration.setSessionFactoryObserver(new SessionFactoryObserver() {
+                @Override
+                public void sessionFactoryCreated(SessionFactory factory) {
+
+                }
+
+                @Override
+                public void sessionFactoryClosed(SessionFactory factory) {
+                    ((StandardServiceRegistryImpl)serviceRegistry).destroy();
+                }
+            });
+           return configuration.buildSessionFactory(serviceRegistry);
+        }
+        catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
-        return sf;
     }
 
-    public Session currentSession() throws HibernateException {
-        Session s = (Session) session.get();
-        // Open a new Session, if this Thread has none yet
-        if (s == null) {
-            s = sessionFactory.openSession();
-            session.set(s);
-        }
-        return s;
-    }
 
-    public  void close(){
-        if(serviceRegistry!= null) {
-            StandardServiceRegistryBuilder.destroy(serviceRegistry);
-        }
-        Session s = (Session) session.get();
-        session.set(null);
-        if (s != null){
-            s.close();
-        }
-    }
 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
