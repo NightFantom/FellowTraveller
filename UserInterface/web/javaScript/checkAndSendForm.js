99e5e270988f7e2812bcab767cf5900e40588314
NightFantom <Den1-1@mail.ru>
2014-05-22 20:22:09 +0400

    Signed-off-by: NightFantom <Den1-1@mail.ru>

--- UserInterface/web/javaScript/checkAndSendForm.js
+++ UserInterface/web/javaScript/checkAndSendForm.js
@@ -2,7 +2,12 @@
  * Created by Виктор on 20.05.2014.
  */
 function checkAndSend(url, form, id) {
-    if (formIsCorrect(form)) {
-        postAjax(url, form, id);
-    }
+  //  document.getElementById(id).innerHTML = '<img src="images/ajaxloaderwheel.gif" >';
+   // if (
+  formIsCorrect(form, id)//) {
+   // getBody(form,id);
+       postAjax(url, form, id);
+  //  }else{
+  //      document.getElementById(id).innerHTML = '<img src="images/ajaxloaderwheel.gif" >';
+ //   }
 }
--- UserInterface/web/javaScript/checkForm.js
+++ UserInterface/web/javaScript/checkForm.js
@@ -1,34 +1,59 @@
 /**
  * Created by Виктор on 20.05.2014.
  */
-function formIsCorrect() {
-    var arrayOfElementsOfForm = getArrayOfElements();
-    var successful = false;
-    for (var i = 0; i < arrayOfElementsOfForm.length; i++) {
-        switch (arrayOfElementsOfForm[i].type) {
-            case 'checkbox':
-                if (arrayOfElementsOfForm[i].value == 'agree') {
-                    successful = true;
-                } else {
-                    successful = false;
-                }
 
-                break;
-
-            default:
-                if (arrayOfElementsOfForm[i].value != "" && arrayOfElementsOfForm[i].value != null) {
-                    successful = true;
-                } else {
-                    successful = false;
-                }
-                break;
-        }
-        if (!successful) {
-            document.getElementById(arrayOfElementsOfForm[i].id).innerHTML = "Введите данные!!";
-            return;
-        }
-
-
+function formIsCorrect(form, id) {
+//    var success= false;
+//    var currentElement;
+//    document.getElementById(id).innerHTML = '<img src="images/ajaxloaderwheel.gif" >';
+//    for (var i = 0; i < form.length; i++) {
+//        var temporaryElement = form[i];
+//        switch (temporaryElement.type) {
+//
+//            case 'checkbox' :
+//                if (temporaryElement.checked) {
+//                    success=true;
+//                } else {
+//                    success=false;
+//                    return;
+//                }
+//                break;
+//            default:
+//                if( currentElement.value.toString()!="" &&currentElement.value.toString()!=null){
+//                    success=true;
+//                 }else{
+//                    success=false;
+//                    return;
+//                }
+//                 break;
+//        }
+//
+//    }
+//    return success;
+    var element = form.getElementById("from");
+    if(element==""){
+        form.getElementById(id).innerHTML = '<img src="images/ajaxloaderwheel.gif" >';
     }
-    return successful;
 }
+
+//function getBody(oForm,id) {
+//    var success= false;
+//   // var aParams = new Array();
+//    for (var i = 0; i < oForm.elements.length; i++) {
+////        var sParam = encodeURIComponent(oForm.elements[i].name);
+////        sParam += "=";
+////        sParam += encodeURIComponent(oForm.elements[i].value);
+//        if(oForm.elements[i].value!=""&&oForm.elements[i].value!=null){
+//            success=true;
+//        }else{
+//            var success= false;
+//            return;
+//        }
+//   //     aParams.push(sParam);
+//    }
+//    if(!success){
+//        document.getElementById(id).innerHTML = '<img src="images/ajaxloaderwheel.gif" >';
+//    }
+//    document.getElementById(id).innerHTML = '<img src="images/ajaxloaderwheel.gif" >';
+////    return aParams.join("&");
+//}
