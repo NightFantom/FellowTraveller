57fa440f007977c08d86b2c27913e21def93d9ec
Denis <Den1-1@mail.ru>
2014-05-23 16:30:05 +0400

    Написаны коммиты.

--- AdminPanel/src/panel/settings/StopHibernate.java
+++ AdminPanel/src/panel/settings/StopHibernate.java
@@ -1,7 +1,38 @@
 package panel.settings;
 
+import hibernateServise.HibernateUtil;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.BaseAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
- * Created by Денис on 23.05.14.
+ * Класс остановки Hibernate
  */
-public class StopHibernate {
+public class StopHibernate extends BaseAction {
+    private static final String FORWARD_SUCCESS = "successful";
+
+    /**
+     * Метод закрывает ссесию Hibernate
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        MessageForm messageForm = (MessageForm) form;
+        try {
+            HibernateUtil.close();
+            messageForm.setMessage("Hibernate остановлен");
+        }catch (Exception e){
+            messageForm.setMessage(e.getMessage());
+        }
+        return mapping.findForward(FORWARD_SUCCESS);
+    }
 }
