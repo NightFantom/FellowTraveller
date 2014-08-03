d069ac0ff6d2fca549e9fca2d5375b598e1c0e52
Denis <Den1-1@mail.ru>
2014-08-04 01:02:55 +0400

    1) Добавлена возможность создания БД 2) Исправлена отсутствие поля "Who".

--- AdminPanel/src/panel/settings/CreateTable.java
+++ AdminPanel/src/panel/settings/CreateTable.java
@@ -39,33 +39,43 @@ public class CreateTable extends BaseAction{
 
         Driver driver = new com.mysql.jdbc.Driver();
         DriverManager.registerDriver(driver);
-
         Connection connection = null;
         ResultSet cursor = null;
 
         try {
             connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-            String send = "CREATE TABLE `ways`  (\n" +
-                    "\t`id`                \tint(11) NOT NULL,\n" +
-                    "\t`startpoint`        \tvarchar(50) NOT NULL,\n" +
-                    "\t`finishpoint`       \tvarchar(50) NOT NULL,\n" +
-                    "\t`intermediatepoints`\tvarchar(100) NOT NULL,\n" +
-                    "\t`day`               \tint(11) NOT NULL,\n" +
-                    "\t`month`             \tint(11) NOT NULL,\n" +
-                    "\t`vkid`              \tvarchar(50) NOT NULL,\n" +
-                    "\t`information`       \tvarchar(300) NULL,\n" +
-                    "\tPRIMARY KEY(`id`)\n" +
-                    ")\n" +
-                    "AUTO_INCREMENT = 0";
-            ResultSet resultSet = connection.createStatement().executeQuery("SHOW TABLES LIKE 'ways'");
-            if (resultSet != null && resultSet.next()){
+            cursor = connection.createStatement().executeQuery("SHOW DATABASES LIKE 'togetherinway'");
+            if (cursor != null && cursor.next()){
+                messageForm.setMessage("База togetherinway уже существует");
+            }else {
+                String send = "CREATE DATABASE `togetherinway`\n" +
+                        "\tDEFAULT CHARACTER SET utf8\n" +
+                        "\tDEFAULT COLLATE utf8_general_ci";
+                connection.createStatement().execute(send);
+                messageForm.setMessage("База togetherinway создана успешно");
+            }
+            cursor = connection.createStatement().executeQuery("SHOW TABLES LIKE 'ways'");
+            if (cursor != null && cursor.next()){
                 messageForm.setMessage("Таблица ways уже существует");
             }else {
+                String send = "CREATE TABLE `ways`  (\n" +
+                        "\t`id`                \tint(11) NOT NULL,\n" +
+                        "\t`startpoint`        \tvarchar(50) NOT NULL,\n" +
+                        "\t`finishpoint`       \tvarchar(50) NOT NULL,\n" +
+                        "\t`intermediatepoints`\tvarchar(100) NOT NULL,\n" +
+                        "\t`day`               \tint(11) NOT NULL,\n" +
+                        "\t`month`             \tint(11) NOT NULL,\n" +
+                        "\t`vkid`              \tvarchar(50) NOT NULL,\n" +
+                        "\t`information`       \tvarchar(300) NULL,\n" +
+                        "\t`who`                \tint(11) NOT NULL,\n" +
+                        "\tPRIMARY KEY(`id`)\n" +
+                        ")\n" +
+                        "AUTO_INCREMENT = 0";
                 connection.createStatement().execute(send);
                 messageForm.setMessage("Таблица ways создана успешно");
             }
         }catch (SQLException e){
-            messageForm.setMessage("Не удалось создать таблицу ways\n" + e.getMessage());
+            messageForm.setMessage(e.getMessage());
         }
         finally {
             if (cursor != null)
